a623deed0d65d02040319cf8f29b8c564b6f2518
Ana <amarcosestevez@gmail.com>
2022-09-24 14:10:04 +0200

    add a new test on Calculator Component

--- tests/components/Calculator.test.jsx
+++ tests/components/Calculator.test.jsx
@@ -1,25 +1,41 @@
 import {CalculatorComponent} from '../../src/components/CalculatorComponent';
 import { render, screen, fireEvent } from '@testing-library/react';
 describe('Test on CalculatorComponent', () => {
-    test('Debe mostrar el componente inicial', ()=>{
+    test('It should display the initial component', ()=>{
         const {container} = render(
             <CalculatorComponent/>
         );
         expect(container).toMatchSnapshot();
       //  screen.debug();
     })
-    test('Si hago la suma de 2 + 6 el texto del resultado debe ser 8',()=>{
+    test('If I do the sum of 2 + 6 the text of the result must be 8',()=>{
         render(<CalculatorComponent/>);
         const btnValueTwo = screen.getByRole('button', {name:2});
         const btnValueSix = screen.getByRole('button', {name:6});
-        const btnSumOperator = screen.getByRole('button', {name:'+'});
+        const btnAddOperator = screen.getByRole('button', {name:'+'});
         const btnEqual = screen.getByRole('button', {name:'='});
         const textResult = screen.getByText('Result:');
 
         fireEvent.click(btnValueTwo);
-        fireEvent.click(btnSumOperator);
+        fireEvent.click(btnAddOperator);
         fireEvent.click(btnValueSix);
         fireEvent.click(btnEqual);
         expect(textResult.innerHTML).toContain('8');
     })
+    test('If I do the division of 10 by 5 the result should be 2 ', () => { 
+        render(<CalculatorComponent/>);
+        const btnValueOne = screen.getByRole('button', {name:1});
+        const btnValueZero = screen.getByRole('button', {name:0});
+        const btnValueFive = screen.getByRole('button', {name:5});
+        const btnSubOperator = screen.getByRole('button', {name:'/'});
+        const btnEqual = screen.getByRole('button', {name:'='});
+        const textResult = screen.getByText('Result:');
+
+        fireEvent.click(btnValueOne);
+        fireEvent.click(btnValueZero);
+        fireEvent.click(btnSubOperator);
+        fireEvent.click(btnValueFive);
+        fireEvent.click(btnEqual);
+        expect(textResult.innerHTML).toContain('2');
+     })
 });
