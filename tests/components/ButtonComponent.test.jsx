7eac70dcebb4c0e6cf64daf21f4c05e4363f4ce1
Ana <amarcosestevez@gmail.com>
2022-09-24 12:51:59 +0200

    add two test on ButtonComponent

---
+++ tests/components/ButtonComponent.test.jsx
@@ -0,0 +1,24 @@
+import { ButtonComponent } from "../../src/components/ButtonComponent";
+import { render, screen, fireEvent } from '@testing-library/react';
+
+describe('Test on the ButtonComponent', () => {
+
+    const initialValue = 2;
+    const isDisabled = false;
+    const action = jest.fn()
+
+    test('The initial component should be displayed', () => { 
+        const { container } = render(
+            <ButtonComponent value={initialValue} disabled={isDisabled} action={action}/>
+        );
+        expect(container).toMatchSnapshot();
+        
+     })
+     test('The function should be called when pressing the button', ()=>{
+        render(<ButtonComponent value={initialValue} disabled={isDisabled} action={action}/>
+        );
+        const btn = screen.getByRole('button');
+        fireEvent.click(btn);
+        expect(action).toHaveBeenCalled();
+     })
+});
